207ba1da9c82a7ca447e83fbe55becc9c9b0e8e4
galloj <jiri.gallo@centrum.cz>
2022-12-20 18:48:49 +0100

    Finnishing

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -165,13 +165,16 @@ public class GameManager : MonoBehaviour
 		 *       WebGL memory or just refreshing the page by reloading the 
 		 *       current URL ("OpenURL", "absoluteURL").
          */
-        
+        Debug.Log("AAAAA");
 #if UNITY_EDITOR
         // Quitting in Unity Editor: 
+        UnityEditor.EditorApplication.isPlaying = false;
 #elif UNITY_WEBPLAYER || UNITY_WEBGL
         // Quitting in the WebGL build: 
+        Application.OpenURL(Application.absoluteURL);
 #else // !UNITY_WEBPLAYER
         // Quitting in all other builds: 
+        Application.Quit();
 #endif
     }
 }
